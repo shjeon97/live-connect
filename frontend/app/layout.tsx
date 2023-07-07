5aea5a1b802ec27181b5cfd022c8f9c05ac73404
shjeon97 <shjeon.dev@gmail.com>
2023-07-07 10:51:07 +0900

    2023-07-07v2

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css';
 import { Inter } from 'next/font/google';
 import ReactQueryProvider from '@/components/ReactQueryProvider';
+import Script from 'next/script';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -16,6 +17,8 @@ export default function RootLayout({
 }) {
   return (
     <html lang="kr">
+      <Script src="https://unpkg.com/vconsole@latest/dist/vconsole.min.js" />
+      <Script id="vconsole"> {`var vConsole = new window.VConsole()`}</Script>
       <body className={inter.className}>
         <ReactQueryProvider>{children}</ReactQueryProvider>
       </body>
--- frontend/components/ReactQueryProvider.tsx
+++ frontend/components/ReactQueryProvider.tsx
@@ -1,12 +1,8 @@
-"use client";
+'use client';
 
-import {
-  QueryClient,
-  QueryClientProvider,
-} from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import { PropsWithChildren, useState } from "react";
-
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { PropsWithChildren, useState } from 'react';
 
 export default function ReactQueryProvider({ children }: PropsWithChildren) {
   const [queryClient] = useState(() => new QueryClient());
