25f76d891f3b2d28e40a8f988ac98bb007cfeecb
shjeon97 <shjeon.dev@gmail.com>
2023-07-09 08:09:26 +0900

    2023-07-09v1

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,7 +1,6 @@
 import './globals.css';
 import { Inter } from 'next/font/google';
 import ReactQueryProvider from '@/components/ReactQueryProvider';
-import Script from 'next/script';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -17,8 +16,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang="kr">
-      <Script src="https://unpkg.com/vconsole@latest/dist/vconsole.min.js" />
-      <Script id="vconsole"> {`var vConsole = new window.VConsole()`}</Script>
       <body className={inter.className}>
         <ReactQueryProvider>{children}</ReactQueryProvider>
       </body>
