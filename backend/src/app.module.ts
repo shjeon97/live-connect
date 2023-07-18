81c4f853978d398b05c93c084f57bced00d3bc8a
shjeon97 <shjeon.dev@gmail.com>
2023-07-18 16:32:00 +0900

    2023-07-18v1

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,6 +3,7 @@ import { ConfigModule } from '@nestjs/config';
 // 타입스크립트로 되어있지 않은 라이브러리 사용시
 import * as Joi from 'joi';
 import { SocketIoModule } from './socket-io/socket-io.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
@@ -17,7 +18,35 @@ import { SocketIoModule } from './socket-io/socket-io.module';
         NODE_ENV: Joi.string()
           .valid('development', 'production', 'test')
           .required(),
+        DB_HOST: Joi.string().required(),
+        DB_PORT: Joi.string().required(),
+        DB_USERNAME: Joi.string().required(),
+        DB_PASSWORD: Joi.string().required(),
+        DB_NAME: Joi.string().required(),
+        PRIVATE_KEY: Joi.string().required(),
+        PORT: Joi.string().required(),
+        SERVER_ADDRESS: Joi.string().required(),
+        GMAIL_SMTP_NAME: Joi.string().required(),
+        GMAIL_SMTP_KEY: Joi.string().required(),
       }),
+    }),
+    // typeORM 정의
+    TypeOrmModule.forRoot({
+      // DB 종류
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      // 계정 정보
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      // DB 이름
+      database: process.env.DB_NAME,
+      // 마이그레이션
+      synchronize: process.env.NODE_ENV !== 'production',
+      // DB로그
+      logging: process.env.NODE_ENV !== 'production',
+      // 사용할 entity들 선언
+      entities: [],
     }),
     SocketIoModule,
   ],
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { json, urlencoded } from 'express';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -26,6 +27,16 @@ async function bootstrap() {
       credentials: true,
     });
   }
+
+  const config = new DocumentBuilder()
+    .setTitle('live-connect API')
+    .setDescription('live-connect API description')
+    .setVersion('1.0')
+    .addTag('live-connect')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   try {
     await app.listen(process.env.PORT, '0.0.0.0', () =>
