58bbcf1ff895c84ed399d6cd704cb93b8045afa2
shjeon97 <shjeon.dev@gmail.com>
2023-07-22 10:15:37 +0900

    2022-07-22v2

--- frontend/app/api/socket-io.ts
+++ frontend/app/api/socket-io.ts
@@ -1,8 +1,10 @@
-import { io } from "socket.io-client";
+import { io } from 'socket.io-client';
 
 export const socket = io(
-  process.env.APP_HOST ? process.env.APP_HOST : "http://localhost:4000",
+  process.env.NEXT_PUBLIC_APP_HOST
+    ? process.env.NEXT_PUBLIC_APP_HOST
+    : 'http://localhost:4000',
   {
-    transports: ["websocket"],
-  }
+    transports: ['websocket'],
+  },
 );
